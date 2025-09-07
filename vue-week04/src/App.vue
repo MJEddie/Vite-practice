9a7faed40390e2db6c4b08c5e331ba41617efc34
Nick <dreadnaughtlin@gmail.com>
2025-09-07 11:30:29 +0800

    init

---
+++ vue-week04/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="container py-4">
+    <div class="row">
+      <!-- 商品列表區 -->
+      <ProductList></ProductList>
+
+      <!-- 購物車區 -->
+      <Cart></Cart>
+    </div>
+
+    <!-- 通知元件 -->
+    <NotificationVue></NotificationVue>
+  </div>
+</template>
+
+<script setup>
+import ProductList from "./components/ProductList.vue";
+import Cart from "./components/Cart.vue";
+import Notification from "./components/Notification.vue";
+</script>
