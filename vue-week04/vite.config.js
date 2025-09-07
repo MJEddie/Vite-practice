7a99e854136b46d5826d3749c2c1eee7e66e4640
Nick <dreadnaughtlin@gmail.com>
2025-09-07 14:33:42 +0800

    update base path from /week04/ to /week4/

--- vue-week04/vite.config.js
+++ vue-week04/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/Vite-practice/week04/',
+    base: '/Vite-practice/week4/',
     plugins: [
         vue(),
         vueDevTools(),
