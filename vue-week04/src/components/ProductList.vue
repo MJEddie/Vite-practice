e396061350fbc2cff2715e8d53eaf15e6d60c04c
Nick <dreadnaughtlin@gmail.com>
2025-09-07 12:01:26 +0800

    add product data flow using props

--- vue-week04/src/components/ProductList.vue
+++ vue-week04/src/components/ProductList.vue
@@ -2,16 +2,13 @@
   <div class="col-md-8">
     <h2 class="mb-3">商品列表</h2>
     <div class="row">
-      <div class="col-md-4 mb-4">
+      <div v-for="product in products" :key="product.id" class="col-md-4 mb-4">
         <div class="card h-100">
-          <img
-            src="https://images.unsplash.com/photo-1546435770-a3e426bf472b?q=80&amp;w=2065&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.1.0&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            class="card-img-top"
-          />
+          <img :src="product.image" class="card-img-top" />
           <div class="card-body">
-            <h5 class="card-title">耳罩式藍牙耳機</h5>
-            <p class="card-text">舒適配戴，支援降噪技術</p>
-            <p class="fw-bold text-primary">$ 2490</p>
+            <h5 class="card-title">{{ product.name }}</h5>
+            <p class="card-text">{{ product.description }}</p>
+            <p class="fw-bold text-primary">$ {{ product.price }}</p>
             <button class="btn btn-success w-100">加入購物車</button>
           </div>
         </div>
@@ -20,4 +17,11 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const props = defineProps({
+  products: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
