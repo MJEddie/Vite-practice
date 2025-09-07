3cff709f84aeaf2d4166ee8960e9fc30023c42cc
Nick <dreadnaughtlin@gmail.com>
2025-09-07 19:37:51 +0800

    add remove todo function

--- vue-week05/src/components/TodoForm.vue
+++ vue-week05/src/components/TodoForm.vue
@@ -1,44 +1,10 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div class="inputBox">
+    <input type="text" placeholder="請輸入待辦事項" />
+    <a href="#">
+      <i class="fa fa-plus"></i>
+    </a>
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
+<script setup></script>
